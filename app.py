7ca60b6836f8f6a6b67bf4118b33b48bf2306246
SauravTelge <18sauravt@gmail.com>
2023-04-19 14:51:46 -0400

    updated data

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 from data_access.tree import BSTNode
 import string
 from data_API.api_housing import housing
+from data_API.api_weather import Weather
+from datetime import date
+
 app = Flask(__name__)
 
 @app.route("/")  
@@ -21,26 +24,42 @@ def results():
                 univcity =  data[i]['university town']
     
          
-        test_str = univcity.translate(str.maketrans('', '', string.punctuation))        
+        test_str = univcity.translate(str.maketrans('', '', string.punctuation))   
+        test_str = test_str.strip()     
         with open('crimes.json','r') as myfile:  
             data = json.load(myfile)  
+ 
         for i in range(len(data)):
-            if test_str == data[i]['city name']:
-                city_rank =  int(data[i]['City rank'])
-                city_crimes_per_day = int(data[i]['Crimes per day'])
+           
+            if test_str == data[i]['city name'].strip():
+                print('yes')
+                city_rank =  float(data[i]['City rank'])
+                city_crimes_per_day = float(data[i]['Crimes per day'])
                 state = data[i]['State']
-        with open('postal.json','r') as myfile:  
-            data = json.load(myfile)
-        for i in range(len(data)):
-            if data[i]['state'] == state:
-                postal = data[i]['postal']
-        response=housing.house(test_str,postal)
-        
+                break
+            else:
+                city_rank = 'No data available'
+                city_crimes_per_day = 'No data available'
+                state = 'No data available'
+        if state == 'No data available':
+            housing_data = 'No data available'
+            weather_data = 'No data available'
+
+        else:
+            with open('postal.json','r') as myfile:  
+                data = json.load(myfile)
+            for i in range(len(data)):
+                if data[i]['state'] == state:
+                    postal = data[i]['postal']
+            housing_data = housing.house(test_str,postal)
+            
+            today = date.today()
+
+            
+            weather_data = Weather.weather_data('ann arbor',str(today.year-3)+'-'+str(today.month)+'-'+str(today.day)+"T00:00:00",str(today)+"T00:00:00")	
 
         
-
-        
-    return render_template('data.html',univname = test_str)
+    return render_template('data.html',univname = test_str,city_rank=city_rank,city_crimes_per_day=city_crimes_per_day,housing_data=housing_data,weather_data=weather_data)
 
 
 if __name__ == "__main__":
--- data_API/api_weather.py
+++ data_API/api_weather.py
@@ -4,19 +4,31 @@ import json
 
 class Weather:
     
-    def weather_data(city):
-        url = f"https://foreca-weather.p.rapidapi.com/location/search/{city}"
-        querystring = {"lang":"en","country":"us"}
-        headers = {
-			"X-RapidAPI-Key": "74b52ccc3dmshc5135a0cc09a5bbp16423ejsnf1ae02f6dcea",
-			"X-RapidAPI-Host": "foreca-weather.p.rapidapi.com"
-		}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        response = response.json()
-        response['locations'][0]
-        return None
+    def weather_data(city,startdate,enddate):
+            url_history = "https://visual-crossing-weather.p.rapidapi.com/history"
+            querystring_history = {"startDateTime":startdate,"aggregateHours":"24","location":city,"endDateTime":enddate,"unitGroup":"us","dayStartTime":"8:00:00","contentType":"json","dayEndTime":"17:00:00","shortColumnNames":"0"}
+ 
+            url_forecast = "https://visual-crossing-weather.p.rapidapi.com/forecast"
+            querystring_forecast = {"aggregateHours":"24","location":city,"contentType":"json","unitGroup":"us","shortColumnNames":"0"}
+            headers = {
+				"X-RapidAPI-Key": "74b52ccc3dmshc5135a0cc09a5bbp16423ejsnf1ae02f6dcea",
+				"X-RapidAPI-Host": "visual-crossing-weather.p.rapidapi.com"
+			}
+            response_forecast = requests.request("GET", url_forecast, headers=headers, params=querystring_forecast).json()
+            
+            response_history = requests.request("GET", url_history, headers=headers, params=querystring_history).json()
+            
+        
+            return (response_forecast,response_history)
+    
+            
 
-		
+			
+			
+
+			
+
+				
 
                 
 		
